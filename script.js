9d0c79cb93562b5d78f98098030433a90aeea9e6
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-03-10 17:55:36 +0200

    Cleared UI state

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
+
+
 
 function addItem(e) {
   e.preventDefault();
@@ -20,7 +23,9 @@ function addItem(e) {
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the dom
   itemList.appendChild(li);
+  checkUI()
 
   itemInput.value = '';
 }
@@ -42,23 +47,37 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if(e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove()
+
+    if(confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove()
+      checkUI()
+    }
   }
 }
 
-console.dir(itemList)
-
 function clearItems() {
-
-  itemList.remove()
-  
-//  while(itemList.firstChild) {
-//    console.log(itemList.childNodes)
-//   itemList.removeChild(itemList.firstChild)
-//  }
+  while(itemList.firstChild) {
+   console.log(itemList.childNodes)
+  itemList.removeChild(itemList.firstChild)
+ }
 }
-// Event Listeners
 
+
+function checkUI() {
+  const items = document.querySelectorAll('li')
+
+if(items.length === 0) {
+  clearBtn.style.display = 'none'
+  itemFilter.style.display = 'none'
+} else {
+  clearBtn.style.display = 'block'
+  itemFilter.style.display = 'block'
+}
+}
+
+// Add event listener
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems)
+
+checkUI()
